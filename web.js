9d00635024659a2d51ef708e93991668dd3c5cc8
Audrey Tsang Ubuntu <audreytsang@gmail.com>
2014-03-10 08:18:05 +0000

    trying to fix bug with web.js

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  var msg = "";
+  var msg = "woopee";
 
-  fs.readFile('index.html', function (err, data) {
-    if (err) throw err;
-    msg = data.toString('utf-8');
-   });
+//  fs.readFileSync('index.html', function (err, data) {
+//    if (err) throw err;
+//    msg = data.toString('utf-8');
+//    console.log(msg);
+//  });
 
   response.send(msg);
 });
